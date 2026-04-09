8f2aa2835ce6a706a10a4de6f78217e16fc52887
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-04-09 12:10:48 +0500

    sound added

--- cooldesign/cools.js
+++ cooldesign/cools.js
@@ -1,30 +1,43 @@
-const displayCount  = document.getElementById("countDisplay");
+const displayCount = document.getElementById("countDisplay");
 const countDec = document.getElementById("dec");
 const countInc = document.getElementById("inc");
 const countReset = document.getElementById("cont-reset")
+const myAudio = new Audio("imgs/bubble-pop.mp3");
 let count = 0;
 
 
 
-countDec.addEventListener("click", function(){
+
+countDec.addEventListener("click", function () {
     count--;
     displayCount.textContent = count;
+    myAudio.currentTime = 0;
+    myAudio.volume = 0.1;
+    myAudio.duration = 0.4;
+    myAudio.play();
 
-    
 
 });
 
 
-countInc.addEventListener("click", function(){
-  count++;
+countInc.addEventListener("click", function () {
+    count++;
     displayCount.textContent = count;
+    myAudio.currentTime = 0;
+    myAudio.volume = 0.1;
+    myAudio.duration = 0.4;
+    myAudio.play();
 })
 
 
 
-countReset.addEventListener("click", function(){
-  count = 0;
+countReset.addEventListener("click", function () {
+    count = 0;
     displayCount.textContent = count;
+    myAudio.currentTime = 0;
+    myAudio.volume = 0.1;
+    myAudio.duration = 0.4;
+    myAudio.play();
 })
 
 
