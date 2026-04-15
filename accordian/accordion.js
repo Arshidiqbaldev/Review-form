556567f411f1bc2f2dfc1d9c2022b7d5030e115a
Arshidiqbaldev <arshidiqbalnet@gmail.com>
2026-04-15 11:38:37 +0500

    accordion

--- accordian/accordion.js
+++ accordian/accordion.js
@@ -1,17 +1,23 @@
-const questions = document.querySelectorAll(".question");
 
-questions.forEach(function (question) {
-    question.addEventListener("click", () => {
-        const item = question.parentElement;
-        const isOpen = item.classList.contains("open");
-        document.querySelectorAll(".item").forEach(i => {
-            i.classList.remove("open");
-        })
-        if (!isOpen) item.classList.add("open");
 
+
+try {
+
+    const questions = document.querySelectorAll(".question");
+    questions.forEach(function (question) {
+        question.addEventListener("click", () => {
+            const item = question.parentElement;
+            const isOpen = item.classList.contains("open");
+            document.querySelectorAll(".item").forEach(i => {
+                i.classList.remove("open");
+            })
+            if (!isOpen) item.classList.add("open");
+
+        });
 
     });
-});
 
+} catch (error) {
+    console.error(error);
 
-
+}
